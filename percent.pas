4c9ff44c903ed3ed91cd0fc998eea355975e04b0
FullmetalCoder <60969891+FullmetalNeverCore@users.noreply.github.com>
2021-11-11 02:33:33 +0200

    Update percent.pas

--- percent.pas
+++ percent.pas
@@ -2,17 +2,17 @@ program CUBE;
 uses CRT;
 var
 m:double; //SUM 
-i:integer; //PERCENT
-n:double; //YEARS 
+i:double; //PERCENT
+n:integer; //YEARS 
 //CYCLES
 x:integer;
 y:longint;
 begin
 writeln('SEND SUM');read(m);writeln('PERCENT ');read(i);writeln('YEARS ');read(n);
-n:=(n/100);
-for x:=i downto 1 do 
+i:=(i/100);
+for x:=n downto 1 do 
  begin
- m:= m+m*n;writeln(m:0:2);
+ m:= m+m*i;writeln(m:0:2);
  end;
 writeln('FINAL RESULT ');writeln(m:0:2);
 for y:=10 downto 1 do Delay(100);
