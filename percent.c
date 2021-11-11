1ae83c9d87df7eaef93f3b9ff90f8575280c4299
FullmetalCoder <60969891+FullmetalNeverCore@users.noreply.github.com>
2021-11-11 05:45:05 +0200

    Update percent.c

--- percent.c
+++ percent.c
@@ -6,14 +6,8 @@ int main(){
     float m,i;
     
     printf("SEND SUM");scanf("%f", &m);printf("PERCENT ");scanf("%f", &i);printf("YEARS ");scanf("%d", &n);
-    printf("%f \n",m);
     i = i/100;
-    printf("%f \n",i);
-    printf("%d \n", n);
-    for (x = 0; n>x; x = x + 1) {
-        m = m+m*i;
-        printf("%d \n",(int)m);
-    }
+    for (x = 0; n>x; x = x + 1) {m = m+m*i;printf("%d \n",(int)m);}
     sleep(5);
     return 0; 
 }
