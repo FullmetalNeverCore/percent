625dd59732cc64e374eda20362259d8597121ef5
FullmetalCoder <60969891+FullmetalNeverCore@users.noreply.github.com>
2021-11-12 09:46:47 +0200

    Update percent.pas

--- percent.pas
+++ percent.pas
@@ -12,8 +12,8 @@ writeln('SEND SUM');read(m);writeln('PERCENT ');read(i);writeln('YEARS ');read(n
 i:=(i/100);
 for x:=n downto 1 do 
  begin
- m:= m+m*i;writeln(m:0:2);
+ m:= m+m*i;writeln(m:0:0);
  end;
-writeln('FINAL RESULT ');writeln(m:0:2);
+writeln('FINAL RESULT ');writeln(m:0:0);
 for y:=10 downto 1 do Delay(100);
 end.
